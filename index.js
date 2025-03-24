5eae5327c1ab845ebfe1ebc88a88c8721fd7ad39
Tulio <tulioluv@gmail.com>
2025-03-24 13:47:59 -0300

    feat: Now the user can calculate, clear and display

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const display =document.getElementById("painel")
+const painel = document.getElementById("painel");
 
-function aparecerDisplay(input){
-    painel.value += input;
+function aparecerDisplay(input) {
+  painel.value += input;
+}
+
+function limparPainel(){
+    painel.value = "";
+
+
+}
+
+function calcular(){
+    painel.value = eval(painel.value);
+    
+
 }
